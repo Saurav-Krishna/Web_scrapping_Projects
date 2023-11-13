9aba97209f58eb5c1c2b016aa4cb8885101d0392
Saurav-Krishna <143007423+Saurav-Krishna@users.noreply.github.com>
2023-11-14 01:58:09 +0530

    completed basic test

--- Basic_Web_Scrapping_beautiful_soup/main.py
+++ Basic_Web_Scrapping_beautiful_soup/main.py
@@ -13,6 +13,38 @@ file_path = os.path.join("F:\Web_scrapping_projects\Web_scrapping_Projects\Basic
 
 with open(file_path,"r") as html_file:
     content = html_file.read()
-    print(content)
+    # print(content)
 
 # using beautiful library to pretify the html file to use different tags 
+
+# creating an instance of beautiful soup 
+
+    soup = BeautifulSoup(content,'lxml') 
+# secondargumetn isto specify the parser method
+# sincewe we want the content as strings used lxml parser to read html
+
+# print(soup.prettify()) # exsactly like html 
+
+# grabbing some tags from html pages
+
+
+
+# tags = soup.find("h2")
+# print(tags)
+
+
+
+# we have to itterate through each h2 tags 
+
+# find method: searches for the first instance of the <h2> tag to find all 
+# we will simply use file_all method
+
+
+    tags = soup.find_all("h2")
+
+    for tag in tags:
+        print(tag)
+        # still we are getting tags , but we want only texts
+
+    for tag in tags:
+        print(tag.text)  # its msilib module in-built
